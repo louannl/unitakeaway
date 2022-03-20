bb572773f4b4d52f576e5c30b3f7dd86b53147e0
louannl <louann_loizou@hotmail.com>
2022-03-20 17:17:24 +0000

    made min order and delivery charge go to the bottom of the minorder component

--- src/pages/ContactUs/MinOrder.js
+++ src/pages/ContactUs/MinOrder.js
@@ -1,27 +1,28 @@
-import tw from '../../helpers/tailwind';
+import tw from "../../helpers/tailwind"
 
 const MinOrder = (props) => {
   return (
     <div
       className={tw(
-        'border-2',
-        'border-uni-green',
-        'shadow-md',
-        'rounded-sm',
-        'text-center',
-        'm-2',
-        'bg-white',
-        'w-full',
-        'p-2',
-        'lg:w-1/3',
-        'xl:w-1/5'
+        "border-2",
+        "border-uni-green",
+        "shadow-md",
+        "rounded-sm",
+        "text-center",
+        "m-2",
+        "bg-white",
+        "w-full",
+        "p-2",
+        "lg:w-1/3",
+        "xl:w-1/5",
+        "flex flex-col"
       )}
     >
-      <p>{props.counties}</p>
+      <p className="h-full p-2">{props.counties}</p>
       <p className="text-uni-red">MINIMUM ORDER £{props.minimum}</p>
       <p className="font-semibold">+ £{props.charge} Delivery charge</p>
     </div>
-  );
-};
+  )
+}
 
-export default MinOrder;
+export default MinOrder
